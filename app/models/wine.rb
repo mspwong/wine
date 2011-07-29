5da8cbf7ba12fdcedaa2f901b9547e28407c1062
Mary Wong <mwong@Mary-Wongs-iMac.local>
2011-07-29 00:39:24 -0700

    wine scaffold (MVC, view helpers, routes, migration script, fixture data, tests to come)
    mysql configurations

---
+++ app/models/wine.rb
@@ -0,0 +1,7 @@
+class Wine < ActiveRecord::Base
+  validates_presence_of :name
+  validates_length_of :name, :maximum => 30
+  validates_numericality_of :vintage, :only_integer => true, :allow_nil => true
+  validates_presence_of :item_no
+  validates_numericality_of :item_no, :only_integer => true
+end
---
+++ db/migrate/20110729050702_create_wines.rb
@@ -0,0 +1,17 @@
+class CreateWines < ActiveRecord::Migration
+  def self.up
+    create_table :wines do |t|
+      t.integer "id", :null=>false
+      t.string "name", :null=>false
+      t.string "varietal"
+      t.integer "vintage"
+      t.string "region"
+      t.integer "item_no", :null=>false
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :wines
+  end
+end
