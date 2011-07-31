52439819d05f99c39bdb7459f85ef744fc36578a
Mary Wong <mwong@Mary-Wongs-iMac-2.local>
2011-07-30 22:31:30 -0700

    left out of previous commit.

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,7 @@
 class Review < ActiveRecord::Base
+  validates_presence_of :reviewer
+  validates_length_of :reviewer, :maximum => 50
+  validates_length_of :body, :maximum => 500
+
   belongs_to :wine
 end
