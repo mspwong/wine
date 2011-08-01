f655fff693c30e821eab5f8d1a18685a10890d01
Mary Wong <mwong@Mary-Wongs-iMac-2.local>
2011-07-31 19:21:56 -0700

    added annotate_models plugin.
    annotated all models (impacted fixture yml).

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+# Schema version: 20110731053333
+#
+# Table name: reviews
+#
+#  id         :integer(4)      not null, primary key
+#  reviewer   :string(255)     not null
+#  body       :text
+#  wine_id    :integer(4)      not null
+#  created_at :datetime        not null
+#  updated_at :datetime        not null
+#
+
 class Review < ActiveRecord::Base
   validates_presence_of :reviewer
   validates_length_of :reviewer, :maximum => 50
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+# Schema version: 20110731053333
+#
+# Table name: tags
+#
+#  id         :integer(4)      not null, primary key
+#  name       :string(255)     not null
+#  wine_id    :integer(4)      not null
+#  created_at :datetime        not null
+#  updated_at :datetime        not null
+#
+
 class Tag < ActiveRecord::Base
   belongs_to :wine
 end
