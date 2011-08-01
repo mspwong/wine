abf748a44ae412c44ba6f6f76a46ccbc985e8ec1
Mary Wong <mwong@Mary-Wongs-iMac-2.local>
2011-08-01 12:11:37 -0700

    added db seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #   
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Major.create(:name => 'Daley', :city => cities.first)
+
+
+primus = Wine.find_by_name("Veramonte Primus") || Wine.new(:name=>"Veramonte Primus")
+primus.varietal = "Other Red Wine"
+primus.vintage = "2007"
+primus.region = "Chile, South America"
+primus.item_no = 99604
+primus.save!
+
+tilia = Wine.find_by_name("Tilia") || Wine.new(:name=>"Tilia")
+tilia.varietal = "Cabernet Sauvignon"
+tilia.vintage = "2009"
+tilia.region = "Argentina, South America"
+tilia.item_no = 105748
+tilia.tags = [Tag.new(:name=>"big, bold, showy", :wine=>tilia)]
+tilia.save!
+
+mary_review = Review.new
+mary_review.reviewer = "Mary"
+mary_review.body = "Big and bold"
+mary_review.wine = tilia
+mary_review.save!
+
+jari_review = Review.new
+jari_review.reviewer = "Jari"
+jari_review.body = "Over the top"
+jari_review.wine = tilia
+jari_review.save!
+
+
