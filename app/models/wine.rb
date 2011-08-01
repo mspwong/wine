a22d8813deff74c72799c58ea2ee6e6aa42ab509
Mary Wong <mwong@Mary-Wongs-iMac-2.local>
2011-08-01 16:18:51 -0700

    tweaked model validations and view labels

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -14,14 +14,16 @@
 #
 
 class Wine < ActiveRecord::Base
+  has_many :reviews
+  has_many :tags
+
   validates_presence_of :name
   validates_length_of :name, :maximum => 30
   validates_numericality_of :vintage, :only_integer => true, :allow_nil => true
   validates_presence_of :item_no
   validates_numericality_of :item_no, :only_integer => true
-
-  has_many :reviews
-  has_many :tags
+  validates_associated :reviews
+  validates_associated :tags
 
   accepts_nested_attributes_for :tags, :allow_destroy => true,
                                 :reject_if => proc { |attrs| attrs.all? { |k, v| v.blank? }}
