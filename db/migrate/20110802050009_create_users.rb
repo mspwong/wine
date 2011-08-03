6c1e06bab54f5aa8c847780b7d779f62d7f65c00
Mary Wong <mwong@Mary-Wongs-iMac-2.local>
2011-08-02 18:45:47 -0700

    deleted primary key from migration scripts.
    tweaked view labels.

--- db/migrate/20110802050009_create_users.rb
+++ db/migrate/20110802050009_create_users.rb
@@ -1,7 +1,6 @@
 class CreateUsers < ActiveRecord::Migration
   def self.up
     create_table :users, :force=>true do |t|
-      t.integer "id", :null=>false
       t.string "name", :null=>false
       t.string "email"
       t.timestamps :null=>false
