b58ab426fe0409e5387016fb5bf5b5fe52a40747
Mary Wong <mwong@Mary-Wongs-iMac-2.local>
2011-08-03 16:41:21 -0700

    refactored fibnonacci

--- app/models/util.rb
+++ app/models/util.rb
@@ -1,15 +1,11 @@
 class Util
 
   def self.fibonacci(input)
-    sequence = [0, 1]
-
     return input if input <= 1
 
-    while sequence.last <= input
-      sequence[sequence.size] = sequence[sequence.size-2] + sequence.last
-    end
+    sequence = [0, 1]
+    sequence << sequence[sequence.size-2] + sequence.last while sequence.last <= input
 
     sequence[sequence.size-2]
-
   end
 end
