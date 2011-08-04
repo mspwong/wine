7f7faa3f220989bf494ac2528f9cada195402422
Mary Wong <mwong@Mary-Wongs-iMac-2.local>
2011-08-03 23:55:47 -0700

    wine unit test.  test fixtures.

--- Gemfile
+++ Gemfile
@@ -8,8 +8,9 @@ gem 'unicorn', '3.6.2'
 gem 'rake', '0.9.1', :require => false
 gem 'rdoc'
 
+gem 'awesome_print', :require => 'ap'
+
 group :development do
-  gem 'awesome_print', :require => 'ap'
   gem 'ruby-debug-base', '0.10.4'
   gem 'ruby-debug-ide', '0.4.10', :require => false
 end
--- test/unit/wine_test.rb
+++ test/unit/wine_test.rb
@@ -1,8 +1,49 @@
 require 'test_helper'
 
 class WineTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+
+ test "find non existing wine" do
+    wine = Wine.find_by_name("Not to be found")
+    assert !wine
   end
+
+  test "find wine by name" do
+    wine = Wine.find_by_name("Veramonte Primus")
+    assert wine
+  end
+
+  test "save wine with duplicate name" do
+    wine = Wine.new(:name => "Veramonte Primus")
+    assert !wine.save
+  end
+
+  test "save wine without item no" do
+    wine = Wine.new(:name => "Test wine")
+    assert !wine.save
+  end
+
+  test "save wine with non-integer item no" do
+    wine = Wine.new(:name => "Test wine", :item_no => "i'm not an integer")
+    assert !wine.save
+  end
+
+  test "save valid wine" do
+    wine = Wine.new(:name => "Test wine", :item_no => 12345)
+    assert wine.save
+  end
+
+  test "update wine synchronizes its reviews status" do
+    wine = wines(:primus)
+    assert wine.active
+    assert wine.reviews
+    assert_equal 2, wine.reviews.size
+    wine.reviews.each { |r| assert r.active }
+
+    wine.update_attribute(:active, false)
+    assert !wine.active
+    assert wine.reviews
+    assert_equal 2, wine.reviews.size
+    wine.reviews.each { |r| assert !r.active }
+  end
+
 end
