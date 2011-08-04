f47a03980eb0e1f9d9be28110ed0f96f03d167eb
Mary Wong <mwong@Mary-Wongs-iMac-2.local>
2011-08-04 00:55:22 -0700

    added test setup

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -2,28 +2,30 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
 
-  test "find user's reviews excludes reviews that are inactive'" do
-    user = users(:wine_spectator)
-    assert_equal 2, user.reviews.size
-    assert user.reviews.first.active
+  def setup
+    @user = users(:wine_spectator)
+  end
 
-    user.reviews.first.update_attribute(:active, false)
-    user.reload
-    assert_equal 1, user.reviews.size
+  test "find user's reviews excludes reviews that are inactive'" do
+    assert_equal 2, @user.reviews.size
+    assert @user.reviews.first.active
+
+    @user.reviews.first.update_attribute(:active, false)
+    @user.reload
+    assert_equal 1, @user.reviews.size
   end
 
   test "update user synchronizes its reviews status" do
-    user = users(:wine_spectator)
-    assert user.active
-    assert user.reviews
-    assert_equal 2, user.reviews.size
-    user.reviews.each { |r| assert r.active }
+    setup do
+      assert @user.active
+      assert_equal 2, @user.reviews.size
+      @user.reviews.each { |r| assert r.active }
+    end
 
-    user.update_attribute(:active, false)
-    assert !user.active
-    assert user.reviews
-    assert_equal 2, user.reviews.size
-    user.reviews.each { |r| assert !r.active }
+    @user.update_attribute(:active, false)
+    assert !@user.active
+    assert_equal 2, @user.reviews.size
+    @user.reviews.each { |r| assert !r.active }
   end
 
 end
