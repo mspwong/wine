df3a91a4b552fc4eede4e599052bcf9796a081c0
Mary Wong <mwong@Mary-Wongs-iMac-2.local>
2011-08-08 03:51:35 -0700

    refactored tests for Integer extension

--- test/unit/integer_test.rb
+++ test/unit/integer_test.rb
@@ -19,38 +19,45 @@ class IntegerTest < ActiveSupport::TestCase
   context "regression testing:  " do
     context "an integer/fixnum/bignum" do
       should "retain core Integer behavior" do
-        assert 4.is_a?(Integer)
-        assert 4.integer?
-        assert 4 == 4
-        assert 4.eql?(4)
-        four = 4
-        assert 4 == four
-        assert 4.eql?(four)
-        assert 4 == 4.0
-        assert !4.eql?(4.0)
-        assert 3 < 4
-        assert_equal 4, 4.round
-        assert_equal 4, 4.ceil
-        assert_equal 1, 4.modulo(3)
-        assert_equal "4", 4.to_s
-        assert_equal 99, -99.abs
-        assert 88.even?
-        assert !89.even?
-        assert !88.odd?
-        assert 89.odd?
-        assert -88.even?
-        assert !-88.odd?
-
-        test_fixnum = 99999999
+        test_fixnum = 4
         assert_equal Fixnum, test_fixnum.class
-        assert test_fixnum.is_a?(Integer)
         assert test_fixnum.is_a?(Fixnum)
+        assert test_fixnum.is_a?(Integer)
+        assert test_fixnum.is_a?(Numeric)
+        assert test_fixnum.is_a?(Precision)
+        assert test_fixnum.is_a?(Object)
+        assert test_fixnum.integer?
+        assert test_fixnum == 4
+        assert test_fixnum.eql?(4)
+        four = 4
+        assert test_fixnum == four
+        assert test_fixnum.eql?(four)
+        assert test_fixnum == 4.0
+        assert !test_fixnum.eql?(4.0)
+        assert !test_fixnum.zero?
+        assert test_fixnum < test_fixnum + 1
+        assert test_fixnum > test_fixnum - 1
+        assert_equal 4, test_fixnum.round
+        assert_equal 4, test_fixnum.ceil
+        assert_equal 1, test_fixnum.modulo(3)
+        assert_equal "4", test_fixnum.to_s
+        assert_equal 2, test_fixnum / 2
+        assert_equal -4, (test_fixnum * -1)
+        assert_equal test_fixnum, (test_fixnum * -1).abs
+        assert test_fixnum.even?
+        assert !test_fixnum.odd?
+        assert (test_fixnum * -1).even?
+        assert !(test_fixnum * -1).odd?
         assert_not_nil test_fixnum.hash
+
         test_bignum = 999999999999999999999999999999
         assert_equal Bignum, test_bignum.class
-        assert test_bignum.is_a?(Integer)
         assert test_bignum.is_a?(Bignum)
+        assert test_bignum.is_a?(Integer)
+        assert test_bignum < test_bignum + 1
+        assert test_bignum > test_bignum - 1
         assert_not_nil test_bignum.hash
+
         assert Time.now.to_i.is_a?(Integer)
       end
     end
@@ -88,10 +95,12 @@ class IntegerTest < ActiveSupport::TestCase
       assert_equal 13, 15.closest_fibonacci_smaller_or_equal_to
       assert_equal 13, 20.closest_fibonacci_smaller_or_equal_to
       assert_equal 21, 21.closest_fibonacci_smaller_or_equal_to
+
       test_fixnum = 99999999
       assert test_fixnum.is_a?(Fixnum)
       assert !test_fixnum.is_a?(Bignum)
       assert_equal 63245986, test_fixnum.closest_fibonacci_smaller_or_equal_to
+
       test_bignum = 999999999999999999999999999999
       assert !test_bignum.is_a?(Fixnum)
       assert test_bignum.is_a?(Bignum)
