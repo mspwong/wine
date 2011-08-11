b241a07e49d3b150d5cb711157328f64c1d0dfb2
Mary Wong <mspwong@hotmail.com>
2011-08-11 00:05:31 -0700

    adding new tag on show wine page.  WIP

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,10 +3,8 @@
 
 $(function() {
     $("#add_tag").click(function(){
-//        var sibling = $(this).prev().prev();
-//        sibling.css("background-color", "red");
-//        var tag = $('input[id="wine_tags_attributes_1_name"');
-        var tag = $('p.my_tag input[type="text"]').last();
-        tag.css("background-color", "red");
+        var tag = $('p.my_tag').last();
+        var num_tags = $('p.my_tag').length;
+        tag.parent().append('<p class="my_tag"><input id="wine_tags_attributes_' + num_tags + '_name" name="wine[tags_attributes][' + num_tags + '][name]" size="30" type="text" value=""><label for="wine_tags_attributes_' + num_tags + '__destroy">Remove:</label><input name="wine[tags_attributes][' + num_tags + '][_destroy]" type="hidden" value="0"><input id="wine_tags_attributes_' + num_tags + '__destroy" name="wine[tags_attributes][' + num_tags + '][_destroy]" type="checkbox" value="1"></p>');
     });
 })
