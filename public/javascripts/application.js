c9486d1212f221086ce4f50661e9f6ae4cce21d3
Mary Wong <mspwong@hotmail.com>
2011-08-10 23:37:01 -0700

    adding new tag on show wine page.  WIP

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,12 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+$(function() {
+    $("#add_tag").click(function(){
+//        var sibling = $(this).prev().prev();
+//        sibling.css("background-color", "red");
+//        var tag = $('input[id="wine_tags_attributes_1_name"');
+        var tag = $('p.my_tag input[type="text"]').last();
+        tag.css("background-color", "red");
+    });
+})
