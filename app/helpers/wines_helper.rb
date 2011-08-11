4e4582db45f73e95d6cde756ff26ea3484ac8a8f
Mary Wong <mspwong@hotmail.com>
2011-08-11 00:23:29 -0700

    more friendly display of tags.

--- app/helpers/wines_helper.rb
+++ app/helpers/wines_helper.rb
@@ -1,2 +1,15 @@
 module WinesHelper
+
+  def format_tags(tags)
+    first = true
+    tags.collect do |t|
+        if first
+            first = false
+            t.name
+        else
+            " | " + t.name
+        end
+    end
+  end
+
 end
