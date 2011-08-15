2def7a1103be169f4376c69010e9812580d81feb
Mary Wong <mspwong@hotmail.com>
2011-08-14 17:34:53 -0700

    jquery local success handler calls global success handler

--- public/javascripts/why_not.js
+++ public/javascripts/why_not.js
@@ -1,83 +1,76 @@
 $(function() {
     $.ajaxSetup({
         async: true,
-        timeout: 50000,              // for server-side debug during dev
+//        timeout: 2000,
         dataType: 'json',
+        global: true,
         beforeSend: function() {
             $("#ajax_response").html("");
         },
-//        success: function(data, textStatus, jqXHR) {
-//            alert("global success handler");
-//            if (jqXHR.status == 204) {
-//                alert("no data found");
-//                return;
-//            }
-//        },
+        success: function(textStatus, xmlHttpRequest, ajaxOptions) {
+            if (xmlHttpRequest.status == 204) {
+                alert("no data found");
+                return true;
+            }
+            return false;
+        },
         error: function(jqXHR, textStatus, errorThrown) {
             alert("ajax error: " + errorThrown + ",     status code: " + jqXHR.status);
         }
     });
 
-    var handleNoData = function(data, textStatus, jqXHR) {
-        if (jqXHR.status == 204) {
-            alert("no data found");
-            return true;
-        }
-        return false;
-    }
-
-    $("#get_wines").click(function(){
+    $("#get_wines").click(function() {
         $.ajax({
-          url: '/why_nots/get_wines',
-          type: 'GET',
-          success: function(data, textStatus, jqXHR) {
-            if (handleNoData(data, textStatus, jqXHR))
-                return;
+            url: '/why_nots/get_wines',
+            type: 'GET',
+            success: function(data, textStatus, jqXHR) {
+                if ($.ajaxSettings.success.call(data, textStatus, jqXHR))
+                    return;
 
-            var dataStr = "";
-            for (var i=0;  i<data.length;  ++i) {
-//                if (i==6) continue;
-                dataStr = dataStr + "<p>" + JSON.stringify(data[i]) + "</p>";
+                var dataStr = "";
+                for (var i = 0; i < data.length; ++i) {
+                    if (i == 6) continue;
+                    dataStr = dataStr + "<p>" + JSON.stringify(data[i]) + "</p>";
+                }
+                $("#ajax_response").html(dataStr);
             }
-            $("#ajax_response").html(dataStr);
-          }
         });
     });
 
-    $("#get_wine").click(function(){
+    $("#get_wine").click(function() {
         var wine_id = $("#wine_id").val().trim();
         if (!wine_id) {
-            alert ("must first enter the wine id");
+            alert("must first enter the wine id");
             return;
         }
         $.ajax({
-          url: '/why_nots/get_wine' + "?id=" + wine_id,
-          type: 'GET',
-          success: function(data, textStatus, jqXHR) {
-              if (handleNoData(data, textStatus, jqXHR))
-                return;
+            url: '/why_nots/get_wine' + "?id=" + wine_id,
+            type: 'GET',
+            success: function(data, textStatus, jqXHR) {
+                if ($.ajaxSettings.success.call(this, textStatus, jqXHR, data))
+                    return;
 
-              $("#ajax_response").html("<p>" + JSON.stringify(data) + "</p>");
-          }
+                $("#ajax_response").html("<p>" + JSON.stringify(data) + "</p>");
+            }
         });
     });
 
-    $("#post_wine").click(function(){
+    $("#post_wine").click(function() {
         var wine_id = $("#post_wine_id").val().trim();
         if (!wine_id) {
-            alert ("must first enter the wine id");
+            alert("must first enter the wine id");
             return;
         }
         $.ajax({
-          url: '/why_nots/post_wine',
-          data: {id: wine_id},
-          type: 'POST',
-          success: function(data, textStatus, jqXHR) {
-            if (handleNoData(data, textStatus, jqXHR))
-                return;
+            url: '/why_nots/post_wine',
+            data: {id: wine_id},
+            type: 'POST',
+            success: function(data, textStatus, jqXHR) {
+                if ($.ajaxSettings.success.call(data, textStatus, jqXHR))
+                    return;
 
-            $("#ajax_response").html("<p>" + JSON.stringify(data) + "</p>");
-          }
+                $("#ajax_response").html("<p>" + JSON.stringify(data) + "</p>");
+            }
         });
     });
 });
