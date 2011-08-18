107f898d82c8316c599780499ee1f1b2cce3d080
Mary Wong <mspwong@hotmail.com>
2011-08-17 20:42:12 -0700

    pig latin:  preserved trailing punctuations

--- app/models/why_nots/pig_latin.rb
+++ app/models/why_nots/pig_latin.rb
@@ -1,9 +1,12 @@
 class PigLatin
   def self.translate(text)
     text.split(" ").collect do |word|
-      chars = word.chars
-      pig_latin_word = (chars.to_a[1, word.length] << chars.first << "ay ").to_s
-      pig_latin_word.capitalize if chars.first == chars.first.capitalize
+      chars = word.downcase.gsub(/[^a-z ]/, '').gsub(/ /, '-').chars
+      pig_latin_ending = "ay "
+      pig_latin_word = (chars.to_a[1, word.length] << chars.first << pig_latin_ending).to_s
+      pig_latin_word.capitalize! if word.first == word.first.capitalize
+      punctuations = word.chars.to_a[pig_latin_word.length-pig_latin_ending.length, word.length].to_s
+      pig_latin_word.chop! << punctuations unless punctuations.blank?
       pig_latin_word
     end.to_s.strip
   end
--- test/unit/why_nots/pig_latin_test.rb
+++ test/unit/why_nots/pig_latin_test.rb
@@ -14,4 +14,10 @@ class PigLatinTest < ActiveSupport::TestCase
     end
   end
 
+  context "'Hello World!!!'" do
+    should "be translated to 'Ellohay Orldway!!!'" do
+      assert_equal 'Ellohay Orldway!!!', PigLatin.translate('Hello World!!!')
+    end
+  end
+
 end
